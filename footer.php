1edf7242ccc9e35c1e143983b57bbca1adc9eed9
Curt Husting <curthusting@gmail.com>
2013-03-14 09:16:18 -0400

    added jquery and moved all script calls below footer instead of in head

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
-<?php
+<footer></footer>
 
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+<?php
+	if (file_exists($folder.'/demo.js')) echo '<script type="text/javascript" href="/'.$folder.'/demo.js"></script>';
 ?>
--- head.php
+++ head.php
@@ -1,9 +1,7 @@
 <head>
 <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
-<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
 <?php
 	if (file_exists($folder.'/demo.css')) echo '<link rel="stylesheet" href="/'.$folder.'/demo.css" type="text/css">';
-	if (file_exists($folder.'/demo.js')) echo '<script type="text/javascript" href="/'.$folder.'/demo.js"></script>';
 ?>
 
 <title>Playground</title>
